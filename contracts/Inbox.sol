ef4d4a728cfd09c21fbf2bf16cd46d306b0c594d
ksinghj <kartarsjabanda@gmail.com>
2022-02-26 18:13:59 +0000

    adds mocha test (start)

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -10,7 +10,5 @@ contract Inbox {
 
     function setMessage(string newMessage) public {
         message = newMessage;
-    }
-
-    
+    }    
 }
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,27 @@
-// contract test code will go here
+const assert = require('assert')
+const ganache = require('ganache')
+const Web3 = require('web3')
+const { interface, bytecode } = require('../compile')
+
+const web3 = new Web3(ganache.provider())
+
+describe('Inbox', () => {
+  let accounts
+  let inbox
+
+  beforeEach(async () => {
+    // Get list of all accounts
+    accounts = await web3.eth.getAccounts()
+
+    // Use one account to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({ data: bytecode, arguments: ['Hi there!'] })
+      .send({ from: accounts[0], gas: '1000000' })
+  })
+
+  describe('Inbox', () => {
+    it('Deploys a contract', () => {
+      console.log('inbox: ', inbox)
+    })
+  })
+})
