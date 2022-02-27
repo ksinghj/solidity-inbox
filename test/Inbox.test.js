6b58d946fa4deca70cca9c994d62097c5dca8bb4
ksinghj <kartarsjabanda@gmail.com>
2022-02-27 10:57:01 +0000

    first functional unit tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -9,19 +9,29 @@ describe('Inbox', () => {
   let accounts
   let inbox
 
+  const mockMessage = 'Hi there!'
+
   beforeEach(async () => {
     // Get list of all accounts
     accounts = await web3.eth.getAccounts()
 
     // Use one account to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-      .deploy({ data: bytecode, arguments: ['Hi there!'] })
+      .deploy({ data: bytecode, arguments: [mockMessage] })
       .send({ from: accounts[0], gas: '1000000' })
   })
 
   describe('Inbox', () => {
     it('Deploys a contract', () => {
       console.log('inbox: ', inbox)
+
+      assert.ok(inbox.options.address)
+    })
+
+    it('Gets initialised with the correct message', async () => {
+      const actualMessage = await inbox.methods.message().call()
+
+      assert.ok(actualMessage === mockMessage)
     })
   })
 })
